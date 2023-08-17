ecb27c45c923c2e3f9cf92b1f141895066bc3b75
Rahul Gupta <88659833+Rahulgupta02@users.noreply.github.com>
2023-08-17 21:47:16 +0530

    Add files via upload

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCSy_ltMlTqcL0FZWjhopSfzxH-fFl93I0",
+    authDomain: "coin-insight.firebaseapp.com",
+    projectId: "coin-insight",
+    storageBucket: "coin-insight.appspot.com",
+    messagingSenderId: "230683411448",
+    appId: "1:230683411448:web:ef0c3e7299f724be070c0d"
+  };
+
+  export default firebaseConfig;
